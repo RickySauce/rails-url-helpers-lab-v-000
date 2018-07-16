9d2dddb82f759c45ac71cf22b949872169147343
Eric DeSimone <eric.desimone@cortland.edu>
2018-07-16 19:00:38 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,10 @@
 class Student < ActiveRecord::Base
   attr_accessor :active
 
+  def after_initialize
+    self.active = false
+  end
+
   def to_s
     self.first_name + " " + self.last_name
   end
