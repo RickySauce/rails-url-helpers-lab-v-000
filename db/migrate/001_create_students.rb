616c4a7e639ebcee684ad749ac3f8945524c3912
Eric DeSimone <eric.desimone@cortland.edu>
2018-07-16 18:51:18 +0000

    automatically backed up by learn

--- db/migrate/001_create_students.rb
+++ db/migrate/001_create_students.rb
@@ -3,7 +3,7 @@ class CreateStudents < ActiveRecord::Migration
     create_table :students do |t|
       t.string :first_name
       t.string :last_name
-      t.boolean :active, :type, default: false 
+      t.boolean active: false
       t.timestamps null: false
     end
   end
