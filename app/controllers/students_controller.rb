17d454b9eb6229c255ff3d7ac8b47d94684a0c09
Eric DeSimone <eric.desimone@cortland.edu>
2018-07-16 18:52:31 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,6 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
@@ -12,5 +12,6 @@ class StudentsController < ApplicationController
 
     def set_student
       @student = Student.find(params[:id])
+      binding.pry
     end
 end
